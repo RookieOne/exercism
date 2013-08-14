0e80bf43cc23a5050c19e91e23550327ab0016b2
Jonathan Birkholz <rookieone@gmail.com>
2013-08-14 10:26:58 -0500

    Refactored ruby bob exercise to use factory

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,16 +1,8 @@
 class Bob
 
-  def hey(message)
-    msg = Message.new(message)
-    if msg.silent?
-      "Fine. Be that way!"
-    elsif msg.yelling?
-      "Woah, chill out!"
-    elsif msg.question?
-      "Sure."
-    else
-      "Whatever."
-    end
+  def hey(message_string)
+    message = Message.new(message_string)
+    TeenagerResponse.new(message).response
   end
 
 end
@@ -34,3 +26,83 @@ class Message
   end
 
 end
+
+class TeenagerResponse
+
+  def initialize(message)
+    @message = message
+    @responses = [SilenceResponse, YellingResponse, QuestionResponse, DefaultResponse]
+    @responses = @responses.map{|r| r.new(message)}
+  end
+
+  def response
+    appropriate_responses = @responses.select {|r| r.appropriate?}
+    appropriate_responses.first.response
+  end
+
+end
+
+class QuestionResponse
+
+  def initialize(message)
+    @message = message
+  end
+
+  def appropriate?
+    @message.question?
+  end
+
+  def response
+    "Sure."
+  end
+
+end
+
+class YellingResponse
+
+  def initialize(message)
+    @message = message
+  end
+
+  def appropriate?
+    @message.yelling?
+  end
+
+  def response
+    "Woah, chill out!"
+  end
+
+end
+
+class SilenceResponse
+
+  def initialize(message)
+    @message = message
+  end
+
+  def appropriate?
+    @message.silent?
+  end
+
+  def response
+    "Fine. Be that way!"
+  end
+
+end
+
+class DefaultResponse
+
+  def initialize(message)
+    @message = message
+  end
+
+  def appropriate?
+    true
+  end
+
+  def response
+    "Whatever."
+  end
+
+end
+
