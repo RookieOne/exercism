42e02a020e7cdaa08a5cdd2363798dfe55324aab
Jonathan Birkholz <rookieone@gmail.com>
2013-08-14 22:21:52 -0500

    iteration 2

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,38 +1,44 @@
-defrecord Message, message: ""
-
 defprotocol MessageType do
   def question?(message)
   def yelling?(message)
   def silent?(message)
 end
 
-defimpl MessageType, for: Message do
+defimpl MessageType, for: BitString do
   def question?(message) do
-    String.ends_with? message.message, "?"
+    String.ends_with? message, "?"
   end
 
   def yelling?(message) do
-    String.upcase(message.message) == message.message
+    String.upcase(message) == message
   end
 
   def silent?(message) do
-    String.strip(message.message) == ""
+    String.strip(message) == ""
   end
 end
 
 defmodule Teenager do
 
   def hey(message) do
-    msg = Message.new(message: message)
+    respond identify_message(message)
+  end
+
+  def respond({:question}), do: "Sure."
+  def respond({:silent}), do: "Fine. Be that way!"
+  def respond({:yelling}), do: "Woah, chill out!"
+  def respond(_), do: "Whatever."
+
+  defp identify_message(message) do
     cond do
-      MessageType.question?(msg) ->
-        "Sure."
-      MessageType.silent?(msg) ->
-        "Fine. Be that way!"
-      MessageType.yelling?(msg) ->
-        "Woah, chill out!"
+      MessageType.question?(message) ->
+        {:question}
+      MessageType.silent?(message) ->
+        {:silent}
+      MessageType.yelling?(message) ->
+        {:yelling}
       true ->
-        "Whatever."
+        {:unidentified}
     end
   end
 
