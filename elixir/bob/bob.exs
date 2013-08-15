02c93ed2ccdbc10c549701f38edaef4278861f6f
Jonathan Birkholz <rookieone@gmail.com>
2013-08-14 19:38:16 -0500

    bob elixir iteration 1

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,11 +1,39 @@
+defrecord Message, message: ""
+
+defprotocol MessageType do
+  def question?(message)
+  def yelling?(message)
+  def silent?(message)
+end
+
+defimpl MessageType, for: Message do
+  def question?(message) do
+    String.ends_with? message.message, "?"
+  end
+
+  def yelling?(message) do
+    String.upcase(message.message) == message.message
+  end
+
+  def silent?(message) do
+    String.strip(message.message) == ""
+  end
+end
+
 defmodule Teenager do
 
   def hey(message) do
-    "Whatever."
-  end
-
-  def question?(message) do
-    
+    msg = Message.new(message: message)
+    cond do
+      MessageType.question?(msg) ->
+        "Sure."
+      MessageType.silent?(msg) ->
+        "Fine. Be that way!"
+      MessageType.yelling?(msg) ->
+        "Woah, chill out!"
+      true ->
+        "Whatever."
+    end
   end
 
 end
