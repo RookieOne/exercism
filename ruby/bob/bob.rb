b793928b7e5e40e777e1e4447eb61d07e7e5e9de
Jonathan Birkholz <rookieone@gmail.com>
2013-08-14 22:35:41 -0500

    reverted to iteration 1 for ruby

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,8 +1,11 @@
 class Bob
 
-  def hey(message_string)
-    message = Message.new(message_string)
-    TeenagerResponse.new.response(message)
+  def hey(message)
+    msg = Message.new(message)
+    return "Fine. Be that way!" if msg.silent?
+    return "Woah, chill out!" if msg.yelling?
+    return "Sure." if msg.question?
+    "Whatever."
   end
 
 end
@@ -11,13 +14,6 @@ class Message
 
   def initialize(message)
     @message = message || ""
-  end
-
-  def message_type
-    return :silent if silent?
-    return :yelling if yelling?
-    return :question if question?
-    :default
   end
 
   def question?
@@ -30,23 +26,6 @@ class Message
 
   def silent?
     @message.strip == ""
-  end
-
-end
-
-class TeenagerResponse
-
-  def responses
-    {
-      silent: "Fine. Be that way!",
-      yelling: "Woah, chill out!",
-      question: "Sure."
-      # default: "Whatever."
-    }
-  end
-
-  def response(message)
-    responses[message.message_type] || "Whatever."  
   end
 
 end
